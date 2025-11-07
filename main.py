685719a279a35addc262e980ff5f3ae523096f95
KHARIWAL <128465167+khariwalhardik@users.noreply.github.com>
2025-11-07 10:38:11 +0530

    feat: add verbose to list token usagge

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import os
 import dotenv
+import sys
+import httpx
 from google import genai
 
 dotenv.load_dotenv()
+api_key=os.getenv("GEMINI_API_KEY")
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+print(sys.argv)
+client = genai.Client(api_key=api_key)
+verbose_flag=False
+if len(sys.argv) < 2:
+    print("I need a prompt!")
+    sys.exit(1)
+
+elif len(sys.argv)==3 and sys.argv[2]=="--verbose":
+    verbose_flag=True
+prompt = sys.argv[1]
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how to find the loop in linked list in python"
-)
+    model="gemini-2.5-flash",
+    contents=[prompt],
+    )
 print(response.text)
+if(verbose_flag):
+    print("prompt token usage:", response.usage_metadata.prompt_token_count)
+    print("thought token usage:", response.usage_metadata.thoughts_token_count)
+    print("completion token usage:", response.usage_metadata.total_token_count)
